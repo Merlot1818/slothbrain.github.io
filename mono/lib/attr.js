f8c789f167b84d62a1f23f9b349c21e2c51d2b3d
slothbrain <shnu_jiefu@hotmail.com>
2017-11-02 17:38:44 +0800

    Site updated: 2017-11-02 17:38:43

---
+++ mono/lib/attr.js
@@ -0,0 +1,17 @@
+var Attribute = function(obj) {
+    this.obj = obj;
+    this.name = obj.name;
+    this.width = obj.width;
+    this.height = obj.height;
+    this.explode = function() {
+        console.log('explode');
+    };
+} 
+
+var Client = function() {
+
+}
+
+var Style = function() {
+    
+}
