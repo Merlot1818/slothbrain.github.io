025e0e71859844922bea13061d7e1a66b008b614
slothbrain <shnu_jiefu@hotmail.com>
2017-11-03 15:54:20 +0800

    Site updated: 2017-11-03 15:54:16

--- mono/lib/attr.js
+++ mono/lib/attr.js
@@ -1,17 +1,110 @@
-var Attribute = function(obj) {
+// var Attribute = function(obj) {
+//     this.obj = obj;
+//     this.name = obj.name;
+//     this.width = obj.width;
+//     this.height = obj.height;
+//     this.explode = function() {
+//         console.log('explode');
+//     };
+// } 
+
+// var Client = function() {
+
+// }
+
+// var Style = function(obj) {
+// 	this.obj = obj;
+// 	this['m.type'] = 'phong';
+// 	this['m.texture.image'] = './images/box.jpg';
+// 	this['m.color'] = 'green';
+// 	this['m.ambient'] = 'green';
+// }
+
+//场景基本属性 SceneAttr
+var SceneAttribute = function(ui,obj,sence) {
+    this.ui = ui;
     this.obj = obj;
-    this.name = obj.name;
-    this.width = obj.width;
-    this.height = obj.height;
-    this.explode = function() {
-        console.log('explode');
-    };
-} 
+    this.sence = sence;
+    var self = this;
+    if(this.ui && this.obj) {
+        for(var o in obj) {
+            self[o] = obj[o];
+        }
+        for(var o in obj) {
+            ui.add(self, o).onChange(function(v) {
+                if(typeof(v) === 'boolean') {// boolean处理
+                    var box = sence.box;
+                    console.log(self.obj);
+                    var s = sence[o];
+                    if(v) {
+                        box.add(s);
+                    }else {
+                        box.remove(s);
+                    }
+                }else {
 
-var Client = function() {
-
+                }
+            });
+        }
+    }
 }
 
-var Style = function() {
-    
+//环境光源属性 AmbientLight
+var AmbientLightAttribute = function(ui,obj,sence) {
+    this.ui = ui;
+    this.obj = obj;
+    this.sence = sence;
+    var self = this;
+    if(this.ui && this.obj) {
+        for(var o in obj) {
+            self[o] = obj[o];
+        }
+        for(var o in obj) {
+            ui.add(self, o).onFinishChange(function(v) {
+                if(typeof(v) === 'boolean') {// boolean处理
+                    var box = sence.box;
+                    var s = sence[o];
+                    if(v) {
+                        box.add(s);
+                    }else {
+                        box.remove(s);
+                    }
+                }else {
+                    var func = mono.setter(this.property);
+                    if(sence.target[func]) {
+                        sence.target[func](new mono.Color('#' + v));
+                    }
+                }
+            });
+        }
+    }
+}
+
+
+//Style属性
+//环境光源属性 AmbientLight
+var StyleAttribute = function(ui,obj,sence) {
+    this.ui = ui;
+    this.obj = obj;
+    this.sence = sence;
+    var self = this;
+    if(this.ui && this.obj) {
+        for(var o in obj) {
+            self[o] = obj[o];
+        }
+        for(var o in obj) {
+            ui.add(self, o, obj[o]).onFinishChange(function(v) {
+                var box = sence.box, property = this.property;
+                box.getDatas().forEach(function(data) {
+                    if(sence.filter && sence.filter(data)) {
+                        if(v === 'null') {
+                            data.setStyle(property, null);
+                        }else {
+                            data.setStyle(property, v);
+                        }
+                    }
+                });
+            });
+        }
+    }
 }
