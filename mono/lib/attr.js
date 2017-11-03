554334aa55a39e5bc488b5441c2ba5774f9bdb2b
slothbrain <shnu_jiefu@hotmail.com>
2017-11-03 22:09:31 +0800

    Site updated: 2017-11-03 22:09:30

--- mono/lib/attr.js
+++ mono/lib/attr.js
@@ -1,41 +1,17 @@
-// var Attribute = function(obj) {
-//     this.obj = obj;
-//     this.name = obj.name;
-//     this.width = obj.width;
-//     this.height = obj.height;
-//     this.explode = function() {
-//         console.log('explode');
-//     };
-// } 
-
-// var Client = function() {
-
-// }
-
-// var Style = function(obj) {
-// 	this.obj = obj;
-// 	this['m.type'] = 'phong';
-// 	this['m.texture.image'] = './images/box.jpg';
-// 	this['m.color'] = 'green';
-// 	this['m.ambient'] = 'green';
-// }
-
 //场景基本属性 SceneAttr
 var SceneAttribute = function(ui,obj,sence) {
     this.ui = ui;
     this.obj = obj;
     this.sence = sence;
+
     var self = this;
     if(this.ui && this.obj) {
         for(var o in obj) {
             self[o] = obj[o];
-        }
-        for(var o in obj) {
-            ui.add(self, o).onChange(function(v) {
+            ui.add(self, o).onFinishChange(function(v) {
                 if(typeof(v) === 'boolean') {// boolean处理
-                    var box = sence.box;
-                    console.log(self.obj);
-                    var s = sence[o];
+                    var box = sence.box, property = this.property;
+                    var s = sence[property];
                     if(v) {
                         box.add(s);
                     }else {
@@ -58,24 +34,72 @@ var AmbientLightAttribute = function(ui,obj,sence) {
     if(this.ui && this.obj) {
         for(var o in obj) {
             self[o] = obj[o];
-        }
-        for(var o in obj) {
-            ui.add(self, o).onFinishChange(function(v) {
-                if(typeof(v) === 'boolean') {// boolean处理
-                    var box = sence.box;
-                    var s = sence[o];
-                    if(v) {
-                        box.add(s);
-                    }else {
-                        box.remove(s);
-                    }
-                }else {
+            if(o === 'color' || o === 'ambient' || o === 'diffuse' || o === 'specular') {
+                ui.addColor(self, o).onFinishChange(function(v) {
                     var func = mono.setter(this.property);
                     if(sence.target[func]) {
-                        sence.target[func](new mono.Color('#' + v));
+                        sence.target[func](new mono.Color(v));
                     }
-                }
-            });
+                });
+            }else {
+                ui.add(self, o).onFinishChange(function(v) {
+                    if(typeof(v) === 'boolean') {// boolean处理
+                        var box = sence.box;
+                        var s = sence[o];
+                        if(v) {
+                            box.add(s);
+                        }else {
+                            box.remove(s);
+                        }
+                    }else {
+                        var func = mono.setter(this.property);
+                        if(sence.target[func]) {
+                            sence.target[func](new mono.Color('#' + v));
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
+
+//点光源属性
+var PointLightAttribute = function(ui,obj,sence) {
+    this.ui = ui;
+    this.obj = obj;
+    this.sence = sence;
+    var self = this;
+    if(this.ui && this.obj) {
+        for(var o in obj) {
+            self[o] = obj[o];
+            if(o === 'color' || o === 'ambient' || o === 'diffuse' || o === 'specular') {
+                ui.addColor(self, o).onFinishChange(function(v) {
+                    var func = mono.setter(this.property);
+                    if(sence.target[func]) {
+                        sence.target[func](new mono.Color(v));
+                    }
+                });
+            }else{
+                // if(o === 'x' || o === 'y' || o === 'z') {
+                //     self[o] = obj[o];
+                // }
+                ui.add(self, o).onFinishChange(function(v) {
+                    if(typeof(v) === 'boolean') {// boolean处理
+                        var box = sence.box;
+                        var s = sence[this.property];
+                        if(v) {
+                            box.add(s);
+                        }else {
+                            box.remove(s);
+                        }
+                    }else {
+                        var func = mono.setter(this.property);
+                        if(sence.target[func]) {
+                            sence.target[func](v);
+                        }
+                    }
+                });
+            }
         }
     }
 }
@@ -90,21 +114,30 @@ var StyleAttribute = function(ui,obj,sence) {
     var self = this;
     if(this.ui && this.obj) {
         for(var o in obj) {
-            self[o] = obj[o];
-        }
-        for(var o in obj) {
-            ui.add(self, o, obj[o]).onFinishChange(function(v) {
-                var box = sence.box, property = this.property;
-                box.getDatas().forEach(function(data) {
-                    if(sence.filter && sence.filter(data)) {
-                        if(v === 'null') {
-                            data.setStyle(property, null);
-                        }else {
-                            data.setStyle(property, v);
+            self[o] = obj[o] instanceof Array ? obj[o][0] : obj[o];
+            if(o === 'm.color' || o === 'm.ambient' || o === 'm.diffuse' || o === 'm.specular') {
+                ui.addColor(self, o).onFinishChange(function(v) {
+                    var box = sence.box, property = this.property;
+                    box.getDatas().forEach(function(data) {
+                        if(sence.filter && sence.filter(data, property)) {
+                            data.setStyle(property, new mono.Color(v));
                         }
-                    }
+                    });
                 });
-            });
+            }else {
+                ui.add(self, o, obj[o]).onFinishChange(function(v) {
+                    var box = sence.box, property = this.property;
+                    box.getDatas().forEach(function(data) {
+                        if(sence.filter && sence.filter(data, property)) {
+                            if(v === 'null') {
+                                data.setStyle(property, null);
+                            }else {
+                                data.setStyle(property, v);
+                            }
+                        }
+                    });
+                });
+            }
         }
     }
 }
